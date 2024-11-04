1a3d60c1e2d840b94c4f13f2b1123432f9625a4d
ifeoluwa <ifeoluwaking24@gmail.com>
2024-11-04 11:46:43 +0000

    update

--- src/api/exercise.ts
+++
@@ -1,9 +0,0 @@
-import { typedApiWrapper } from '../typed';
-import apiHandler from './apiHandler';
-
-export const ExerciseApi = typedApiWrapper({
-  getExercises: async () => apiHandler.get('https://wger.de/api/v2/exercise/'),
-  getExerciseById(id: string) {
-    return apiHandler.get(`https://wger.de/api/v2/exercise/${id}`);
-  },
-});
--- src/start.ts
+++ src/start.ts
@@ -1,5 +1,3 @@
-#!/usr/bin/node
-
 import { initialise } from "./initializer";
 
 initialise();
