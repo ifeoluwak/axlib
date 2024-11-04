1afbe1a83a5a7f909c9f49b325a7dd2d4194ba11
ifeoluwa <ifeoluwaking24@gmail.com>
2024-11-04 14:46:11 +0000

    update

--- src/start.ts
+++ src/start.ts
@@ -1,13 +1,9 @@
 #!/usr/bin/env node
 
 import { initialise } from "./initializer.js";
-import { yargs } from "yargs";
 
-const argv = yargs.usage('Usage: $0 [options] inputFile rootName')
-  .alias('i', 'interface-file')
-  .string('i')
-  .describe('i', 'Specify output file for interfaces')
-  .argv
+var argv = require('yargs/yargs')(process.argv.slice(2)).parse();
+
 
 console.log({argv});
 
