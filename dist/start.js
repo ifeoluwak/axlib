c52030e1480dde454eaf9a99b78a63bcb846b77d
ifeoluwa <ifeoluwaking24@gmail.com>
2024-11-06 19:23:09 +0000

    update yargs

--- dist/start.js
+++ dist/start.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
+import yargs from 'yargs';
 import { initialise } from "./initializer.js";
-var argv = require('yargs/yargs')(process.argv.slice(2)).parse();
+const argv = yargs(process.argv.slice(2)).parse();
 console.log({ argv });
 if (argv.init) {
     console.log('got here... express app');
